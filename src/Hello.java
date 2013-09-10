b884315c9e900ed14a0564445ecadc83cb8fa54e
Peter Kriens <peter.kriens@aqute.biz>
2013-09-10 10:32:04 +0200

    Voor  mischa

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
    public static void main(String args[] ) {
-	System.out.println("Hello " + (args.length > 0? args[0] : "Thomas"));
+	System.out.println("Hello " + (args.length > 0? args[0] : "Mischa"));
    }
 }
 
